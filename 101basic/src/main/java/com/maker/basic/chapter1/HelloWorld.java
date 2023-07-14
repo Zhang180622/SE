d944fb925d947f269167c75b10c21cb5cd9c5240
Zhang180622 <252251157@qq.com>
2023-07-14 23:18:06 +0800

    log4j.properties

--- 101basic/src/main/java/com/maker/basic/chapter1/HelloWorld.java
+++ 101basic/src/main/java/com/maker/basic/chapter1/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.maker.basic.chapter1;//包名com.course.day01，所有字母小写
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * @author: maker
  * @date: 2023-06-17 11:20
@@ -8,7 +10,9 @@ package com.maker.basic.chapter1;//包名com.course.day01，所有字母小写
  */
 //类名HelloWorld，大驼峰命名
 public class HelloWorld {
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
     public static void main(String[] args){
+        logger.info("Hello World! --by logger info");
         System.out.println("Hello World!");
     }
 }
