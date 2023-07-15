5b5bf4685da1ffc123ca5ff3becda2e1bc7ac2c9
Zhang180622 <252251157@qq.com>
2023-07-15 13:29:05 +0800

    log4j.properties

--- 101basic/src/main/java/com/maker/basic/chapter1/HelloWorld.java
+++ 101basic/src/main/java/com/maker/basic/chapter1/HelloWorld.java
@@ -12,7 +12,6 @@ import org.slf4j.LoggerFactory;
 public class HelloWorld {
     private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
     public static void main(String[] args){
-        logger.info("Hello World! --by logger info");
-        System.out.println("Hello World!");
+        logger.info("Hello World!");
     }
 }
--- 101basic/src/main/java/com/maker/basic/chapter1/HomeWork.java
+++ 101basic/src/main/java/com/maker/basic/chapter1/HomeWork.java
@@ -1,11 +1,15 @@
 package com.maker.basic.chapter1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author: maker
  * @date: 2023-06-17 18:07
  * @version: 1.0
  */
 public class HomeWork {
+    private static final Logger logger = LoggerFactory.getLogger(HomeWork.class);
     /*
         1、输出各种基本数据类型常量：整数四种进制、浮点数、字符、布尔、字符串；
         2、整数各进制数据转换；
@@ -13,30 +17,30 @@ public class HomeWork {
      */
     public static void main(String[] args) {
         homeWork1();
-        System.out.println("-------分隔符--------");
+        logger.info("-------分隔符--------");
         homeWork3((byte)0x1F);
-        System.out.println("-------分隔符--------");
+        logger.info("-------分隔符--------");
         homeWork3((byte)0x7C);
     }
 
     //输出各种基本数据类型的常量
     public static void homeWork1(){
         //整数 十进制
-        System.out.println(10);
+        logger.info(String.valueOf(10));
         //整数 二进制
-        System.out.println(0b1001);
+        logger.info(String.valueOf(0b1001));
         //整数 十六进制
-        System.out.println(0xF1);
+        logger.info(String.valueOf(0xF1));
         //整数 八进制
-        System.out.println(017);
+        logger.info(String.valueOf(017));
         //小数
-        System.out.println(0.35);
+        logger.info(String.valueOf(0.35));
         //布尔
-        System.out.println(true);
+        logger.info(String.valueOf(true));
         //字符
-        System.out.println('中');
+        logger.info(String.valueOf('中'));
         //字符串
-        System.out.println("UTF-8");
+        logger.info("UTF-8");
     }
 
     //10、2、16、8进制数字转换
@@ -49,7 +53,7 @@ public class HomeWork {
         if(charAt<0){
             return;
         }
-        System.out.println((char)charAt);
+        logger.info(String.valueOf((char)charAt));
     }
 
 }
