d6b36460163cf808bdecfb88f86e5be93ef8d4f5
Zhang180622 <252251157@qq.com>
2023-07-24 21:11:22 +0800

    add Solution14

---
+++ 112LeetCode/src/main/java/com/leetcode/simple/Solution14.java
@@ -0,0 +1,23 @@
+package com.leetcode.simple;
+
+/* *
+ * @Author: zhangsp
+ * @Date: 2023/7/17 18:35
+ * @Version: 1.0
+ * @comment: 最长公共前缀
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+   如果不存在公共前缀，返回空字符串 ""。
+    示例 1：
+        输入：strs = ["flower","flow","flight"]
+        输出："fl"
+    示例 2：
+        输入：strs = ["dog","racecar","car"]
+        输出：""
+        解释：输入不存在公共前缀。
+    链接：https://leetcode.cn/problems/longest-common-prefix
+ */
+public class Solution14 {
+    public String longestCommonPrefixV1(String[] strs) {
+        return "";
+    }
+}
